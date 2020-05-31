fc2384f1b2733a570705c5d1610ef8c83c27b416
Mike Akiba <mike2718@gmail.com>
2020-06-01 00:22:11 +0800

    更新

--- bt.sh
+++ bt.sh
@@ -1,1 +1,1 @@
-transmission-daemon -a 192.168.1.6 -B -C -T -w /home/mike/下载 -o -y -m -ep -GSR
+transmission-daemon -a "192.168.1.*" -B -C -T -w /home/mike/下载 -o -y -m -ep -GSR
--- chrome.sh
+++ chrome.sh
@@ -1,1 +1,1 @@
-google-chrome --password-store=basic
+google-chrome --password-store=basic --proxy-server=socks5://127.0.0.1:7891
